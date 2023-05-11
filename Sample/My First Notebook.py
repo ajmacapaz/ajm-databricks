8e53b891b29c38f7e7f1ec4f9b269b5c1955bfd7
ajmacapaz <ajmacapaz>
2023-05-11 21:15:01 +0000

    My first commit.

---
+++ Sample/My First Notebook.py
@@ -0,0 +1,17 @@
+# Databricks notebook source
+print('Hello, World!');
+print("Welcome to Databricks Lakehouse!");
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC
+# MAGIC Select "This was run using SQL.", "This is a second cell."
+
+# COMMAND ----------
+
+print("This is a third cell.")
+
+# COMMAND ----------
+
+
